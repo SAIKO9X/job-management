583584eec244239bfdc182d22ca65e4b5600b4a9
SAIKO9X <cardosed3@gmail.com>
2024-07-26 02:05:21 -0300

    Criando entidade Company

---
+++ src/main/java/com/job/management/modules/company/entities/Company.java
@@ -0,0 +1,48 @@
+package com.job.management.modules.company.entities;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.validator.constraints.Length;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "tb_company")
+public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Email(message = "O campo [email] deve conter um e-mail válido")
+    private String email;
+
+    @Length(min = 10, max = 100, message = "A senha deve conter entre (10) e (100) caracteres")
+    private String password;
+
+    @NotBlank(message = "Digite o nome da empresa")
+    private String name;
+
+    private String address;
+    private String cnpj;
+    private String phone;
+    private String website;
+    private String description;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
